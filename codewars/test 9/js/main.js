58cab5b34b4374cf6982db8ee2e6e2888013ac0e
JP <jonathankbyers@gmail.com>
2023-11-17 10:38:40 -0500

    updating todays challenges

--- codewars/test 9/js/main.js
+++ codewars/test 9/js/main.js
@@ -1,72 +1,40 @@
 
-// str DNA
-// str RNA
-// 'GCAT' => 'GCAU'
-// taken in the str, replace all 'T's with 'U's, return new rna str
-
-// function dnaToRna(dnaStr){
-//     return dnaStr.replaceAll('T', 'U')
-// }
-
-// let testVar1 = "TGCA"
-// let testVar2 = "aaaxbbbbyyhwawiwjjjwwm" // 
-// console.log(dnaToRna(testVar1));
-// document.querySelector('h2').innerText = dnaToRna(testVar1);
 
 // 0-9
 // String representation
 // 1 = 'One'
-// function numDigitToString(num){
-//     switch(num){
-//         case 0:
-//             return 'Zero'
-//         case 1:
-//             return 'One'
-//         case 2:
-//             return 'Two'
-//         case 3:
-//             return 'Three'
-//         case 4:
-//             return 'Four'
-//         case 5:
-//             return 'Five'
-//         case 6:
-//             return 'Six'
-//         case 7:
-//             return 'Seven'
-//         case 8:
-//             return 'Eight'
-//         case 9:
-//             return 'Nine'
-//         default:
-//             return 'Enter between 0 and 9'
-//     }
-// }
+function numDigitToString(num){
+    switch(num){
+        case 0:
+            return 'Zero'
+        case 1:
+            return 'One'
+        case 2:
+            return 'Two'
+        case 3:
+            return 'Three'
+        case 4:
+            return 'Four'
+        case 5:
+            return 'Five'
+        case 6:
+            return 'Six'
+        case 7:
+            return 'Seven'
+        case 8:
+            return 'Eight'
+        case 9:
+            return 'Nine'
+        default:
+            return 'Enter between 0 and 9'
+    }
+}
 
-// let testVar1 = 7  // "Seven"
-// let testVar2 = "aaaxbbbbyyhwawiwjjjwwm" // 
-// console.log(numDigitToString(testVar1));
-// document.querySelector('h2').innerText = numDigitToString(testVar1);
+let testVar1 = 7  // "Seven"
+let testVar2 = "aaaxbbbbyyhwawiwjjjwwm" // 
+console.log(numDigitToString(testVar1));
+document.querySelector('h2').innerText = numDigitToString(testVar1);
 
-// two numbers: miles away and gallons left
-// true or false can they make it getting 25miles a gallon
-// function canMakeIt(miles, gallons){
-//     const milesToGallon = 25
-//     let dist = milesToGallon * gallons
-//     return dist >= miles
-// }
-
-// let testVar1 = 50  // 
-// let testVar2 = 2 // 
-// console.log(canMakeIt(testVar1, testVar2));
-// document.querySelector('h2').innerText = canMakeIt(testVar1, testVar2);
-
-
-
-let testVar1 = 50  // 
-let testVar2 = 2 // 
-console.log(canMakeIt(testVar1, testVar2));
-document.querySelector('h2').innerText = canMakeIt(testVar1, testVar2);
 
 
 
