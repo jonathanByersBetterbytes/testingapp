3d4f4d7f6dbcd49aef3cbe2c71939836f8c6e4c6
JP <jonathankbyers@gmail.com>
2023-12-10 12:41:58 -0500

    updating todays challenges

--- codewars/war 1/js/main.js
+++ codewars/war 1/js/main.js
@@ -1,19 +1,21 @@
 
-const transformNamesToUppercase = function (names) {
-    const results = [];
-    for (let i = 0; i < names.length; i++) {
-      results.push(names[i].toUpperCase());
-    }
-    return results;
-  };
-  
+// recursion
+function factorial(num){
+    if(num === 0) return 1
+    return num * factorial(num-1)
+}
 
+function strRev(text){
+    if(text.length === 1) return text
+    else return text.slice(-1) + strRev(text.slice(0,-1))
+}
+ 
 const names = ['irish', 'daisy', 'anna'];
 let testVar1 = "The quick brown fox jumps over the lazy dog." // 
 let testVar2 = "This is not a pangram." // 
 let testVar3 = [2,1,1] // 
-console.log(transformNamesToUppercase(names)); // ['IRISH', 'DAISY', 'ANNA'];
-//console.log(pangram(testVar2));
+console.log(factorial(20)); // ['IRISH', 'DAISY', 'ANNA'];
+console.log(strRev('SessionStack'));
 // console.log(diffNum(testVar3) == -1);
 //document.querySelector('h2').innerText = leastNum(testVar2, 'value');
 
