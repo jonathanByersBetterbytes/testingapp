8e34d1288a5647279c8a06fe069cb758419641d1
JP <jonathankbyers@gmail.com>
2023-12-12 13:55:22 -0500

    updating todays challenges

--- codewars/war 1/js/main.js
+++ codewars/war 1/js/main.js
@@ -1,40 +1,18 @@
-// ten minute walk
-// perfect grid roads
-// button press
-// get an array of one letter strings ['n', 's', 'w', 'e']
-// walk one block in the direction of letter
-// one minute for each block
-// return true if exactly 10 minute walk, false if not
-function walk(arr){
-    if(arr.length === 10){
-        let dx = 0
-        let dy = 0
-        arr.forEach(l =>{
-            console.log(l)
-            switch(l){
-                case 'n':
-                    dx++
-                    break
-                case 's':
-                    dx--
-                    break
-                case 'e':
-                    dy++
-                    break
-                case 'w':
-                    dy--
-                    break
-            }
-        })
-        if(dx === 0 && dy === 0)return true
-    }
-    return false
+// takes a name always valid string
+// if name starts with "R" or "r" == plays banjo
+// returns either name + " plays banno" or name + " does not play banjo"
+
+function doesPlayBanjo(name){
+    if(name[0] === 'R' || name[0] === 'r') return name+" plays banjo"
+    else return name + ' does not play banjo'
 }
+
+
 const names = ['irish', 'daisy', 'anna'];
 let testVar1 = "The quick brown fox jumps over the lazy dog." // 
 let testVar2 = "This is not a pangram." // 
 let testVar3 = [2,1,1] // 
-console.log(walk(['n','n','n','e','e','s','s','s','w','w'])); // ['IRISH', 'DAISY', 'ANNA'];
+console.log(doesPlayBanjo('roger')); // ['IRISH', 'DAISY', 'ANNA'];
 //console.log(strRev('SessionStack'));
 // console.log(diffNum(testVar3) == -1);
 //document.querySelector('h2').innerText = leastNum(testVar2, 'value');
