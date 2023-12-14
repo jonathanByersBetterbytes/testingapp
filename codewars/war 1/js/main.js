6064499518fc994c3abfc2beedcc179d3b7eeaf2
JP <jonathankbyers@gmail.com>
2023-12-14 09:50:28 -0500

    updating todays challenges

--- codewars/war 1/js/main.js
+++ codewars/war 1/js/main.js
@@ -1,18 +1,16 @@
-// takes a name always valid string
-// if name starts with "R" or "r" == plays banjo
-// returns either name + " plays banno" or name + " does not play banjo"
 
-function doesPlayBanjo(name){
-    if(name[0] === 'R' || name[0] === 'r') return name+" plays banjo"
-    else return name + ' does not play banjo'
-}
+[1,2,3,4].forEach(function(i){
+    setTimeout(i => console.log('processing sync'), 3000)
+}, 3000)
+
+
 
 
 const names = ['irish', 'daisy', 'anna'];
 let testVar1 = "The quick brown fox jumps over the lazy dog." // 
 let testVar2 = "This is not a pangram." // 
 let testVar3 = [2,1,1] // 
-console.log(doesPlayBanjo('roger')); // ['IRISH', 'DAISY', 'ANNA'];
+//console.log(count(2,5)) //
 //console.log(strRev('SessionStack'));
 // console.log(diffNum(testVar3) == -1);
 //document.querySelector('h2').innerText = leastNum(testVar2, 'value');
