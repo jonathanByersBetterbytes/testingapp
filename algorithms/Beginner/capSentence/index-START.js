567384a9d56b09f078050c71c638e76bb82f09c9
JP <jonathankbyers@gmail.com>
2024-03-10 17:37:42 -0400

    testing

--- algorithms/Beginner/capSentence/index-START.js
+++ algorithms/Beginner/capSentence/index-START.js
@@ -2,20 +2,17 @@
 Given a sentence containing two or more words, 
 return the equivalent of the sentence when capitalised. E.g
   capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+
+USING MAP!
+
 */
-
-
-
 
 function capSentence(text) {
   // make all lowercase, split the string into words
    let words = text.toLowerCase().split(' ')
-   let newSentence = []
-   // for each word capitalize first letter and join with the rest of the word
-   words.forEach(word => {
-    word = word[0].toUpperCase() + word.slice(1)
-    // push word into newSentence array
-    newSentence.push(word)
+   // use .map to form a NEW array and put each word with capitalized first letter and join with the rest of the word
+   let newSentence = words.map(word => {
+    return word[0].toUpperCase() + word.slice(1)
    })
    //return joined newSentence
    return newSentence.join(' ')
@@ -23,9 +20,62 @@ function capSentence(text) {
 
 console.log(capSentence('the tales of scotch!'))
 
-//module.exports = capSentence
+//module.exports = capSentence0
 
 
+
+
+/* CHALLENGE
+Given a sentence containing two or more words, 
+return the equivalent of the sentence when capitalised. E.g
+  capSentence('the tales of scotch!') // would return 'The Tales Of Scotch!' 
+
+USING MAP and SLICE!
+
+*/
+
+function capSentence(text) {
+  // make all lowercase, split the string into words
+   let words = text.toLowerCase().split(' ')
+   // use .map to form a NEW array and put each word with capitalized first letter and join with the rest of the word
+   let newSentence = words.map(word => {
+    return word[0].toUpperCase() + word.slice(1)
+   })
+   //return joined newSentence
+   return newSentence.join(' ')
+}
+
+
+
+
+// USING MAP and SLICE!
+function capSentence(text) {
+  // make all lowercase, split the string into words
+   let words = text.toLowerCase().split(' ')
+   // use .map to form a NEW array and put each word with capitalized first letter and join with the rest of the word
+   let newSentence = words.map(word => {
+    return word[0].toUpperCase() + word.slice(1)
+   })
+   //return joined newSentence
+   return newSentence.join(' ')
+}
+
+
+// the For Each way
+// function capSentence(text) {
+//   // make all lowercase, split the string into words
+//    let words = text.toLowerCase().split(' ')
+//    let newSentence = []
+//    // for each word capitalize first letter and join with the rest of the word
+//    words.forEach(word => {
+//     // push word into newSentence array
+//     newSentence.push(word[0].toUpperCase() + word.slice(1))
+//    })
+//    //return joined newSentence
+//    return newSentence.join(' ')
+// }
+
+// MY FIRST TAKE AT IT
 // function capSentence(text) {
 //   // split the string into words
 //    let words = text.split(' ')
