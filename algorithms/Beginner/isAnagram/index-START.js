c7f9c1404b64337fe329385507aac3d2eb34fb06
JP <jonathankbyers@gmail.com>
2024-03-13 16:58:28 -0400

    algarithms

--- algorithms/Beginner/isAnagram/index-START.js
+++ algorithms/Beginner/isAnagram/index-START.js
@@ -4,12 +4,44 @@ of each other. Return true if the pass the test and false if they
 don't. E.g
     isAnagram('silent', 'listen') // should return true
 */
-
-
-
+// two words
+// are they anograms?
+// 'listen' => 'silent' === true
+// with character map
 function isAnagram(stringA, stringB) {
-    // Code goes here
+    // write a funtion to create a charMap
+    let createCharMap = str => {
+        let charMap = {}
+        for(let char of str){ 
+            if(charMap.hasOwnProperty(char)) charMap[char]++
+            else charMap[char] = 1
+        }
+        return charMap
+    }
+    if(stringA.length === stringB.length){
+        let stringAMap = createCharMap(stringA)
+        let stringbMap = createCharMap(stringB)
+        for(let char in stringAMap)
+            if(stringAMap[char] !== stringbMap[char]) // return are they the same?
+                return false
+        return true
+    }else return false
 }
 
+let result = isAnagram('listen','silent')
+console.log(result)
+document.querySelector('h2').innerText = result
 
-module.exports = isAnagram
+
+// module.exports = isAnagram
+
+// with RegEx
+// function isAnagram(stringA, stringB) {
+//     // write a funtion to clean the strings 
+//     // split, sort, and join
+//     const sanitizeStr = str => str.toLowerCase()
+//                               .replace(/[^a-z\d]/g, '')
+//                               .split('').sort().join('')
+//     // return are they the same?
+//     return sanitizeStr(stringA) == sanitizeStr(stringB)
+// }
--- algorithms/Beginner/searchReplace/index-START.js
+++ algorithms/Beginner/searchReplace/index-START.js
@@ -1,11 +1,41 @@
+// Receiving a sentence, a word in the sentence and a replacement 
+// for that word as arguments, perform a search and replace on the 
+// sentence using the arguments provided and return the new sentence. E.g
+
+// searchReplace("He is Sleeping on the couch", "Sleeping", "sitting") 
+// should return "He is Sitting on the couch"
+// Note: Make sure to match the casing(capitalization) of the string to be replaced as shown above.
+
+// sentence, word, replaceWord
+// new sentence with word replaced, match the case of word to be replaced
+// "He is Sitting", 'Sitting", "sleeping" => "He is Sleeping"
 
 
+function searchReplace(sent,word,replace){
+    // check for capitalized first letter of word to be replaced
+    if(word[0] === word[0].toUpperCase()){
+        // capitalize the first letter of the replacement word if so
+        // join the first letter of replacement word with the rest of the word
+        replace = replace[0].toUpperCase() + replace.slice(1)
+    }
+    // return the new sentence with word replaced
+    return sent.replace(word, replace)
+}
 
-
-
-
-let result = longestWord('raceicar racer fails to pass the development test')
+let result = searchReplace('raceicar racer Fails to pass the development test', 'Fails','succeeds')
 console.log(result)
 document.querySelector('h2').innerText = result
 
 
+
+// // solve with RegEx
+// function searchReplace(sent, word, replace){    
+//     // declare a new RegEx object from the word to replace
+//     let regExWord = new RegExp(word,'gi')
+//     // check if the first letter of word to replace is capitalized
+//     // if it is then capitalize first letter of replacement word
+//     // create a new replacement word by joining the capitalized letter with the rest of the replacement word
+//     if(/[A-Z]/.test(word[0])) replace = replace[0].toUpperCase()+replace.slice(1)
+//     // return the new sentence by replacing old word
+//     return sent.replace(regExWord, replace)
+// }
