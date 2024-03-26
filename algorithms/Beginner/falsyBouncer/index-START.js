f35cc22305180ec4793bd1113df5e366eef25d58
JP <jonathankbyers@gmail.com>
2024-03-26 16:31:34 -0400

    algorithms

--- algorithms/Beginner/falsyBouncer/index-START.js
+++ algorithms/Beginner/falsyBouncer/index-START.js
@@ -5,10 +5,30 @@ and return an array of only truthy values.
 E.g  falsyBouncer([1, 0, null, '', 5]) // should return [1,5]
 */
 
+// array
+// returns and array with only truthy values
+// [0,false,true,1] [true,1]
 
 function falsyBouncer(array) {
-    // Code goes here
+    // loop with a for of loop
+    let truthyArr = []
+    for(elm of array) if(elm) truthyArr.push(elm)
+    // return result
+    return truthyArr
 }
 
+ 
+let result = falsyBouncer([0,false,true,1])
+console.log(result)
+document.querySelector('h2').innerText = result
 
-module.exports = falsyBouncer
+//module.exports = falsyBouncer
+
+
+// function falsyBouncer(array) {
+//     // loop with a filter
+//     return array.filter(item => {
+//         return item
+//     })
+//     // return result
+// }
--- algorithms/Beginner/mergeArrays/index-START.js
+++ algorithms/Beginner/mergeArrays/index-START.js
@@ -4,20 +4,53 @@
  E.g mergeArrays([1,2,3,3,3], [1,4,5,2]) // should return [1,2,3,4,5]
 */
 
-function mergeArrays(...arrays) {
-
-    let jointArray = []
-    
+function mergeArrays(...arrays){
+    let joinArr = []
     arrays.forEach(array => {
-        jointArray = [...jointArray, ...array]
-    });
+        joinArr = [...joinArr,...array]
+    })
+    let uniqueArr = joinArr.filter((item, index) => {
+        return joinArr.indexOf(item) === index
+    })
+    return uniqueArr
+}
 
-    return [...new Set([...jointArray])]
 
-} 
  
 let result = mergeArrays([1,2,3,3,3], [1,4,5,2])
 console.log(result)
 document.querySelector('h2').innerText = result
  
-module.exports = mergeArrays
+//module.exports = mergeArrays
+
+
+// function mergeArrays(...arrays){
+//     let joinArray = []
+//     arrays.forEach(array => {
+//         joinArray = [...joinArray,...array]
+//     })
+//     let uniqueArr = joinArray.reduce((newArray, item) => {
+//         if(newArray.includes(item)) return newArray
+//         else return [...newArray, item]
+//     }, [])
+//     return uniqueArr
+// }
+
+
+
+
+// function mergeArrays(...arrays){
+//     let joinArray = []
+//     arrays.forEach(array => {
+//         joinArray = [...joinArray,...array]
+//     })
+//     return Array.from(new Set([...joinArray]))
+// }
+
+// function mergeArrays(...arrays){
+//     let joinArray = []
+//     arrays.forEach(array => {
+//         joinArray = [...joinArray,...array]
+//     })
+//     return [...new Set([...joinArray])]
+// }
