00406ff0dcc8eddc11d0ab994ea0b4cc51f56a9e
JP <jonathankbyers@gmail.com>
2024-03-30 10:34:18 -0400

    alg chal

--- algorithms/Beginner/whereIBelong/index-START.js
+++ algorithms/Beginner/whereIBelong/index-START.js
@@ -7,13 +7,33 @@ whereIBelong([1,2,3,4], 1.5) // should return 1 because it is greater
 than 1(index 0), but less than 2(index 1).
 */
 
-
+// array of nums and a single num
+// return the lowest index the num will be when inserted and sorted assending
+//[1,3,5,8], 1.2  == 1
 
 function whereIBelong(arr, num) {
-   // Code goes here
-
+   var counter = 0
+   for (i = 0; i < arr.length; i++) {
+       if (num > arr[i]) {
+           counter++;
+       }
+       //console.log(arr[i])
+   } 
+   return counter
 }
 
 
+let result = whereIBelong([9,3,1,5,8], 1.2)
+console.log(result)
+document.querySelector('h2').innerText = result
 
-module.exports = whereIBelong
+//module.exports = whereIBelong
+
+// function whereIBelong(arr, num) {
+//    // push the num into the array
+//    // sort the array assending
+//    arr.push(num)
+//    arr.sort((a,b) => a-b)
+//    // find the index
+//    return arr.indexOf(num)
+// }
