a323ca8a3151d47fd3e5547e52c237dc14b729df
JP <jonathankbyers@gmail.com>
2024-04-04 16:12:49 -0400

    updates

--- React/src/App.js
+++ React/src/App.js
@@ -48,15 +48,16 @@ export default function Game(){
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[history.length-1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares){
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory([...history, nextSquares]);
+    setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
   function jumpTo(nextMove){
+    setHistory(history.slice(0, nextMove + 1));
     setCurrentMove(nextMove);
   }
 
@@ -64,11 +65,19 @@ export default function Game(){
     let description;
     if(move > 0) description = 'Go to move #' + move;
     else description = 'Go to game start';
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}></button>
-      </li>
-    );
+    if(move === currentMove){
+      return (
+        <li key={move}>
+          You are at move # {currentMove+1}
+        </li>
+      );
+    }else {
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
   });
 
   return (
